eb9ee17db51f1674140d35a2c8403712ef0ba4f9
AbdulmateenAl <alabiabdulmateen94@gmail.com>
2025-06-10 16:45:53 +0100

    done with the models

---
+++ src/core/models/hp.model.ts
@@ -0,0 +1,50 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "src/auth/utils/db";
+
+export class HealthPractitioner extends Model {}
+
+HealthPractitioner.init(
+	{
+		id: {
+			type: DataTypes.UUID,
+			primaryKey: true,
+			defaultValue: DataTypes.UUIDV4,
+		},
+		email: DataTypes.STRING,
+        password: DataTypes.TEXT,
+		email_verified: DataTypes.BOOLEAN,
+		is_verified: DataTypes.BOOLEAN,
+		firstname: DataTypes.STRING,
+		lastname: DataTypes.STRING,
+		passport: DataTypes.TEXT,
+		hp_type_id: { type: DataTypes.UUID, allowNull: false },
+		profile_picture: DataTypes.TEXT,
+		identification_means: DataTypes.ENUM(
+			"PASSPORT", //Added myself
+			"NIN",
+			"IDCARD",
+			"BVN",
+			"Driver's License"
+		),
+		identification_number: { type: DataTypes.STRING, unique: true }, // Added myself
+		identification_verified: {
+			type: DataTypes.BOOLEAN,
+		},
+		refresh_token: DataTypes.TEXT,
+		idcard: DataTypes.TEXT,
+		passport_url: DataTypes.TEXT, // Added myself
+		nin: DataTypes.NUMBER,
+		bvn: DataTypes.NUMBER,
+		license_number: DataTypes.STRING, //Added myself
+		license_expiry_date: DataTypes.DATE, //Added myself
+		available_time: DataTypes.ARRAY(DataTypes.DATE),
+		work_identification_number: DataTypes.STRING,
+		company_name: DataTypes.TEXT,
+		company_address: DataTypes.TEXT,
+	},
+	{
+		sequelize,
+		modelName: "HealthPractitioner",
+		timestamps: true,
+	}
+);
---
+++ src/core/models/request_session.model.ts
@@ -0,0 +1,24 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "src/auth/utils/db";
+
+export class RequestSession extends Model {}
+
+RequestSession.init(
+	{
+		id: {
+			type: DataTypes.UUID,
+			defaultValue: DataTypes.UUIDV4,
+			primaryKey: true,
+		},
+		patient_id: DataTypes.UUID,
+		health_practitioner_id: DataTypes.UUID,
+		status: DataTypes.ENUM("pending", "accepted", "rejected", "cancelled"),
+		patient_symptoms: DataTypes.TEXT,
+		ongoing_medication: DataTypes.TEXT,
+	},
+	{
+		sequelize,
+		modelName: "RequestSession",
+		timestamps: true,
+	}
+);

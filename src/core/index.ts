f11315c3634b71da08b9b3c87ee1bcdb88ba1499
Samuel Iyiola <Samueliyiola13@gmail.com>
2025-06-13 13:57:02 +0100

    made changes

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,9 +1,5 @@
 export * from "./utils";
 // export * from "./common";
-<<<<<<< HEAD
 export * from "./middlewares";
-=======
-// export * from "./middlewares";
 export * from "./config";
 export * from "./models";
->>>>>>> cbc6457cf80db8d47ff569b10ec4d982501a3f72

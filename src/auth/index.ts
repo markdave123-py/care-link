800243bf667eb227170623935abfb3299e23701e
AbdulmateenAl <alabiabdulmateen94@gmail.com>
2025-06-16 13:42:15 +0100

    fix import for hp controller

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,7 +1,7 @@
 export * from "./config";
 export * from "./controllers";
 export * from "./routes";
-// export * from "./services";
+export * from "./services";
 export * from "./types";
 export * from "./utils";
-// export * from "./validation";
+export * from "./validation";

3447e87fcb7264d5be2e4c81771adc508d9bbb90
AbdulmateenAl <alabiabdulmateen94@gmail.com>
2025-06-16 14:33:06 +0100

    corrected validation for registering health practitioner

--- src/auth/routes/patient.routes.ts
+++ src/auth/routes/patient.routes.ts
@@ -3,6 +3,7 @@ import PatientController from "../controllers/patient.controller";
 import { RequestValidator } from "../../core";
 import { loginSchema, registerPatientSchema } from "../validation";
 import AuthController from "../controllers/auth.controller";
+import AuthMiddleware from "../middlewares/auth.middleware";
 
 const patientRouter = Router();
 
@@ -10,5 +11,6 @@ patientRouter.get('/google', AuthController.initializeGoogleAuth);
 patientRouter.get('/google/callback', AuthController.getToken);
 patientRouter.post('/register', RequestValidator.validate(registerPatientSchema), PatientController.register);
 patientRouter.post('/login', RequestValidator.validate(loginSchema), PatientController.login);
+patientRouter.post('/logout', AuthMiddleware.authenticateUser, AuthController.logout);
 
 export default patientRouter;

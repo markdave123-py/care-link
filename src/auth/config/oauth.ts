589ad53ade5f6ae509d83663b336f1ba4e3aabbe
AbdulmateenAl <alabiabdulmateen94@gmail.com>
2025-07-04 08:43:50 +0100

    changed scope, authorization_url, and userinfo to env for oauth

--- src/auth/config/oauth.ts
+++ src/auth/config/oauth.ts
@@ -3,9 +3,13 @@ import type { GoogleOAuthConfig } from "../types/oauth.types";
 
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_HP_REDIRECT_URI, GOOGLE_PATIENT_REDIRECT_URI } = process.env;
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_HP_REDIRECT_URI,
+  GOOGLE_PATIENT_REDIRECT_URI, GOOGLE_SCOPE, GOOGLE_AUTHORIZATION_URL,
+  GOOGLE_TOKEN_ENDPOINT, GOOGLE_USERINFO } = process.env;
 
-if (!GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET || !GOOGLE_HP_REDIRECT_URI || !GOOGLE_PATIENT_REDIRECT_URI) {
+if (!GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET || !GOOGLE_HP_REDIRECT_URI ||
+  !GOOGLE_PATIENT_REDIRECT_URI || !GOOGLE_SCOPE || !GOOGLE_AUTHORIZATION_URL
+  || !GOOGLE_TOKEN_ENDPOINT || !GOOGLE_USERINFO) {
   throw new Error("Missing Google OAuth environment variables");
 }
 
@@ -13,19 +17,19 @@ export const googlePatient: GoogleOAuthConfig = {
   client_id: GOOGLE_CLIENT_ID,
   client_secret: GOOGLE_CLIENT_SECRET,
   redirect_uri: GOOGLE_PATIENT_REDIRECT_URI,
-  scope: "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email",
-  authorization_url: "https://accounts.google.com/o/oauth2/v2/auth",
-  token_url: "https://oauth2.googleapis.com/token",
-  userinfo_url: "https://www.googleapis.com/oauth2/v2/userinfo",
+  scope: GOOGLE_SCOPE,
+  authorization_url: GOOGLE_AUTHORIZATION_URL,
+  token_url: GOOGLE_TOKEN_ENDPOINT,
+  userinfo_url: GOOGLE_USERINFO,
 };
 
 
 export const googleHp: GoogleOAuthConfig = {
-    client_id: GOOGLE_CLIENT_ID || "your-client-id",
-    client_secret: GOOGLE_CLIENT_SECRET || "your-client-secret",
-    redirect_uri: GOOGLE_HP_REDIRECT_URI || 'http://localhost:3000/api/v1/auth/google/callback',
-    scope: 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email',
-    authorization_url: 'https://accounts.google.com/o/oauth2/v2/auth',
-    token_url: 'https://oauth2.googleapis.com/token',
-    userinfo_url: 'https://www.googleapis.com/oauth2/v2/userinfo'
+    client_id: GOOGLE_CLIENT_ID,
+    client_secret: GOOGLE_CLIENT_SECRET,
+    redirect_uri: GOOGLE_HP_REDIRECT_URI,
+    scope: GOOGLE_SCOPE,
+    authorization_url: GOOGLE_AUTHORIZATION_URL,
+    token_url: GOOGLE_TOKEN_ENDPOINT,
+    userinfo_url: GOOGLE_USERINFO
 }

55a6c52f5e61dc871472dac2ff250fe9b74f2bc2
markdave123-py <nwaekwudavid@gmail.com>
2025-07-30 06:15:08 +0100

    eabled ssl for db

--- src/core/config/db.ts
+++ src/core/config/db.ts
@@ -1,6 +1,15 @@
 import { Sequelize } from 'sequelize';
 import { env } from '../../auth/config';
 
-export const sequelize = new Sequelize(env.POSTGRES_URI, {logging: false});
+export const sequelize = new Sequelize(env.POSTGRES_URI, {
+    logging: false,
+    dialect: 'postgres',
+    dialectOptions:{
+        ssl: {
+            require: true,
+            rejectUnauthorized: false    
+        }
+    }
+});
 
 export default sequelize;

a8764c95809e98ec234f9a76c884ef38debd8d2e
AbdulmateenAl <alabiabdulmateen94@gmail.com>
2025-10-02 12:37:39 +0100

    rabbitmq and email changed

--- src/core/config/db.ts
+++ src/core/config/db.ts
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 import { env } from '../../auth/config';
 
 const isProduction = process.env.NODE_ENV === 'production';
-const isLocalDevelopment = process.env.NODE_ENV === 'development' || !process.env.NODE_ENV;
+// const isLocalDevelopment = process.env.NODE_ENV === 'development' || !process.env.NODE_ENV;
 
 export const sequelize = new Sequelize(env.POSTGRES_URI, {
     logging: false,
